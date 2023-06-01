1c859a8c57cbba4a01ce4887dd1c3c5602426698
nyin01 <79818377+nyin01@users.noreply.github.com>
2023-06-01 17:42:21 -0400

    fix errors in slurm demo shell script

--- demo_slurm/pipeline_slurm.sh
+++ demo_slurm/pipeline_slurm.sh
@@ -18,15 +18,17 @@ echo -e "* ${RED}Started: ${GREEN}$timestamp ${RESET}"
 
 echo -e "* ${YELLOW}Generating scripts...${RESET}"
 # prepares the directories and scripts for the lariat mapping run
-python scripts/map_lariats_top_no_slurm.py $INFO_FILE
+python scripts/map_lariats_top.py $INFO_FILE
 
 echo -e "* ${YELLOW}Mapping...${RESET}"
 # Run all read mapping scripts. Each larmap*.sh script will output logs to its own log file within the log_dir/child_logs directory.
-./demo_no_slurm/scripts/bash_all.sh
+./demo_no_slurm/scripts/_sbatch_all.sh
+
+wait  # Wait for all background jobs to finish
 
 echo -e "* ${YELLOW}Integrating results...${RESET}"
 # combines the mapping results from each sample's read one and read two files and performs post-mapping filtering before outputting the final lariat mapping results
-python scripts/merge_filter_lariats_no_slurm.py $INFO_FILE
+python scripts/merge_filter_lariats.py $INFO_FILE
 
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 echo -e "* ${RED}Finished: ${GREEN}$timestamp ${RESET}"
